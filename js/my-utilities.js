36370ef011bc8fc1958c270294fe96144910a5dc
Mathieu Perez <mathieu.perez@gmail.com>
2011-07-17 16:19:50 +0200

    refactore Advanced Options module by using details/summary tags from html5

--- js/my-utilities.js
+++ js/my-utilities.js
@@ -12,21 +12,6 @@ $("nav a").live("click", function(evt){
 	trackAnalytics("clickmenu", $(this).attr("href"));
 });
 
-
-$(".advancedOptionsClosed a").live("click", function(evt){
-	var h3 = $(this).parent();
-	h3.hide();
-	h3.next().show();
-	h3.next().next().show();
-});
-
-$(".advancedOptionsOpened a").live("click", function(evt){
-	var h3 = $(this).parent();
-	h3.hide();
-	h3.prev().show();
-	h3.next().hide();
-});
-
 function parseBool(value) {
   if (typeof value === "string") {
      value = value.replace(/^\s+|\s+$/g, "").toLowerCase();
@@ -39,9 +24,6 @@ function parseBool(value) {
 $(function() {
 	new Base64;
 	new EscapeUtils;
-
-	$(".advancedOptionsOpened").hide();
-	$(".advancedOptions").hide();
 
 	$(".copy").click(function(evt){
 		
